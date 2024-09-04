80b768be056dcf0ebcb86987062f9f4d1f38133f
Emmanuel <emmyprime2015@gmail.com>
2024-09-04 14:14:42 -0700

    removed unwanted files

--- expense-tracker/src/index.js
+++ expense-tracker/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+// Create a root element to render the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
